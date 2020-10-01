33bee1880491889f9c380e9efd9744b5ef59ce53
jtkwon <taerakwon@gmail.com>
2020-10-01 01:45:20 -0400

    Used Cargo to create a new project, build, run, check, and build --release

---
+++ chapter1/main.rs
@@ -0,0 +1,11 @@
+// main is the first code that runs in every executable Rust program
+fn main() {
+    println!("Hello, world!") // This is rust macro.  Without ! is for functions
+}
+
+/**
+ * Use rustc to compile.  It is similar to gcc or clang. 
+ * Since rust is "ahead-of-time" compiled language, you can compile and give program to someone else
+ * After compiling, Rust outputs a binary executable.
+ * Example: rustc main.rs
+ * /
